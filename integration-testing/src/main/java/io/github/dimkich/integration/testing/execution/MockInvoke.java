7d1ac88c258a19c0cc877bdc7b02b9d652977832
dimkich <demin@ster.me>
2025-07-01 16:59:03 +0500

    improve MockInvoke equalsTo method

--- integration-testing/src/main/java/io/github/dimkich/integration/testing/execution/MockInvoke.java
+++ integration-testing/src/main/java/io/github/dimkich/integration/testing/execution/MockInvoke.java
@@ -2,14 +2,27 @@ package io.github.dimkich.integration.testing.execution;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.experimental.Accessors;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonDifferenceCalculator;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicMarkableReference;
 
 @Data
 @Accessors(chain = true)
 public class MockInvoke {
+    private static final RecursiveComparisonDifferenceCalculator compCalculator =
+            new RecursiveComparisonDifferenceCalculator();
+    private static final RecursiveComparisonConfiguration compConfig = new RecursiveComparisonConfiguration();
+
+    static {
+        compConfig.registerEqualsForType((amr1, amr2) -> compCalculator.determineDifferences(amr1.getReference(),
+                amr2.getReference(), compConfig).isEmpty(), AtomicMarkableReference.class);
+    }
+
     @JacksonXmlProperty(isAttribute = true)
     private String name;
     @JacksonXmlProperty(isAttribute = true)
@@ -18,7 +31,32 @@ public class MockInvoke {
     private Object result;
     private Throwable exception;
 
+    @SneakyThrows
     public boolean equalsTo(String name, String method, List<Object> arg) {
-        return Objects.equals(this.name, name) && Objects.equals(this.method, method) && Objects.equals(this.arg, arg);
+        if (!Objects.equals(this.name, name) || !Objects.equals(this.method, method)) {
+            return false;
+        }
+        if (this.arg == null || arg == null) {
+            return this.arg == arg;
+        }
+        if (this.arg.size() != arg.size()) {
+            return false;
+        }
+        for (int i = 0; i < this.arg.size(); i++) {
+            if (!isEquals(this.arg.get(i), arg.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isEquals(Object o1, Object o2) throws NoSuchMethodException {
+        if (o1 == null || o2 == null) {
+            return o1 == o2;
+        }
+        if (o1.getClass() == o1.getClass().getMethod("equals", Object.class).getDeclaringClass()) {
+            return Objects.equals(o1, o2);
+        }
+        return compCalculator.determineDifferences(o1, o2, compConfig).isEmpty();
     }
 }
