3a98be66fb481c4c0ce54762b9f2d21a905b0db8
dimkich <ddouble@ya.ru>
2025-12-16 18:38:19 +0500

    remove integration-testing-compile module since time setup for tests is done by @MockJavaTime

--- integration-testing/src/main/java/io/github/dimkich/integration/testing/IntegrationTestConfig.java
+++ integration-testing/src/main/java/io/github/dimkich/integration/testing/IntegrationTestConfig.java
@@ -2,7 +2,6 @@ package io.github.dimkich.integration.testing;
 
 import io.github.dimkich.integration.testing.assertion.AssertionConfig;
 import io.github.dimkich.integration.testing.date.time.DateTimeService;
-import io.github.dimkich.integration.testing.date.time.TestClockService;
 import io.github.dimkich.integration.testing.execution.MockInvokeConfig;
 import io.github.dimkich.integration.testing.format.TestFormatConfig;
 import io.github.dimkich.integration.testing.initialization.InitializationConfig;
@@ -23,7 +22,7 @@ import java.util.List;
 @Configuration
 @ConditionalOnProperty(value = "integration.testing.enabled", havingValue = "true", matchIfMissing = true)
 @Import({DynamicTestBuilder.class, WaitCompletionConfig.class, StorageConfig.class, DateTimeService.class,
-        InitializationConfig.class, MockInvokeConfig.class, OpenApiConfig.class, AssertionConfig.class, TestClockService.class,
+        InitializationConfig.class, MockInvokeConfig.class, OpenApiConfig.class, AssertionConfig.class,
         WebConfig.class, TestFormatConfig.class})
 public class IntegrationTestConfig {
     @Bean
